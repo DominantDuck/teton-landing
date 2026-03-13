3670493554cd353bdbb1673bdfe810b8efc06bb0
Aiden Gilmore <91899915+DominantDuck@users.noreply.github.com>
2026-03-13 16:55:18 -0400

    Update subscribe.js

--- api/subscribe.js
+++ api/subscribe.js
@@ -55,6 +55,7 @@ export default async function handler(req, res) {
     return res.status(200).json({ success: true });
   } catch (err) {
     console.error('Subscribe error:', err);
-    return res.status(500).json({ error: 'Something went wrong' });
+    return res.status(500).json({ error: 'Something went wrong!' });
+
   }
 }
